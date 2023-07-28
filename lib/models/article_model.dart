2ed61b6cf9cb8d1dcdb518294ce46247f33a66a6
amalmohan123 <amalkarikkad@gmail.com>
2023-07-28 10:34:09 +0530

    second

--- lib/models/article_model.dart
+++ lib/models/article_model.dart
@@ -1,23 +1,21 @@
 import 'dart:core';
 
 class ArticleModel {
-String author;
- String title;
-   String? description;
+  late String author;
+  late String title;
+  late String description;
   late String url;
   late String urlToImage;
   late String content;
   // late DateTime publishedAt;
 
-ArticleModel({
- required this.author, 
-  required this.title,
-  required this.description,
-  required this.url,
-  required this.urlToImage,
-  required this.content,
-  // required this.publishedAt
-
-});
-   
+  ArticleModel({
+    required this.author,
+    required this.title,
+    required this.description,
+    required this.url,
+    required this.urlToImage,
+    required this.content,
+    // required this.publishedAt
+  });
 }
--- lib/views/home.dart
+++ lib/views/home.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:news_api/core/constance.dart';
 import 'package:news_api/helper/data.dart';
+import 'package:news_api/helper/news.dart';
+import 'package:news_api/models/article_model.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -10,15 +12,23 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  List<ArticleModel> articles = [];
+  bool loading = true;
 
-
-@override
+  @override
   void initState() {
     super.initState();
     getCategories();
   }
 
-
+  getNews() async {
+    News newsClass = News();
+    await newsClass.getNews();
+    articles = newsClass.news;
+    setState(() {
+      loading = false;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -40,11 +50,32 @@ class _HomePageState extends State<HomePage> {
           ),
           centerTitle: true,
         ),
-        body: Container(
-          child: Column(
-            children: [],
-          ),
-        ),
+        body: loading
+            ? Center(
+                child: Container(
+                  child: const CircularProgressIndicator(strokeWidth: 4),
+                ),
+              )
+            : Container(
+                child: Column(
+                  children: [
+                    Container(
+                      padding: EdgeInsets.symmetric(horizontal: 15),
+                      height: 70,
+                      child: ListView.builder(
+                          itemCount: categories.length,
+                          shrinkWrap: true,
+                          scrollDirection: Axis.horizontal,
+                          itemBuilder: (context, index) {
+                            return CategoryTile(
+                              imageUrl: categories[index].imageUrl,
+                              categoryName: categories[index].categoryName,
+                            );
+                          }),
+                    )
+                  ],
+                ),
+              ),
       ),
     );
   }
@@ -57,10 +88,60 @@ class CategoryTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Stack(children: [
+    return GestureDetector(
+      onTap: () {},
+      child: Container(
+        margin: const EdgeInsets.only(right: 15),
+        child: Stack(
+          children: [
+            Image.network(
+              imageUrl,
+              width: 120,
+              height: 60,
+              fit: BoxFit.cover,
+            ),
+            Container(
+              alignment: Alignment.center,
+              width: 120,
+              height: 60,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(6),
+                // color: blackColor,
+              ),
+              child: Text(
+                categoryName,
+                style: const TextStyle(
+                  color: whiteColor,
+                  fontWeight: bold,
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
 
-      ],),
+class BlogTile extends StatelessWidget {
+  final String imageUrl, title, desc;
+  const BlogTile({
+    super.key,
+    required this.imageUrl,
+    required this.title,
+    required this.desc,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        children: [
+          Image.network(imageUrl),
+          Text(title),
+          Text(desc),
+        ],
+      ),
     );
   }
 }
